40528f741faab714844400fd89370cce5aed8c66
Claudia Nishimoto Voiski <claudinhan@gmail.com>
2025-01-07 17:13:35 -0300

    versão 1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ car_data = pd.read_csv('./vehicles.csv')
 st.header('Dados de anúncios de vendas de carros')
 
 # Exibindo texto orientativo de funcionamento do sistema
-st.write('**Selecione ao menos uma das opções abaixo para visualizar o respectivo gráfico**')
+st.write('*Selecione ao menos uma das opções abaixo para visualizar o respectivo gráfico')
 
 # Checkbox para gerar um histograma
 hist_checkbox = st.checkbox(
@@ -19,7 +19,7 @@ hist_checkbox = st.checkbox(
 if hist_checkbox:
     # Escrever uma mensagem
     st.write(
-        'Criando um histograma')
+        '*Criando um histograma...*')
 
     # Criar um histograma
     fig = px.histogram(car_data, x='model_year', y='price', color='model')
@@ -27,13 +27,15 @@ if hist_checkbox:
     # Exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
+
 # Checkbox para gerar gráfico de dispersão
 scatter_checkbox = st.checkbox(
     'Criar um gráfico de dispersão de Preço x Ano do modelo')
 
 # Se o checkbox para criar gráfico de dispersão for clicado
 if scatter_checkbox:
-    st.write('Criando um gráfico de dispersão')
+    # Exibir uma mensagem
+    st.write('*Criando um gráfico de dispersão...*')
 
     # Criar um gráfico de dispersão
     fig = px.scatter(car_data, x='model_year', y='price', color='model')
