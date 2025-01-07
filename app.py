458aaebf1faa131abef7d6d70dc08b7aa8295eb9
Claudia Nishimoto Voiski <claudinhan@gmail.com>
2025-01-07 17:05:51 -0300

    versão 1

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-car_data = pd.read_csv('./vehicles.csv')  # Lendo csv com os dados de veículos
+# Lendo csv com os dados de veículos
+car_data = pd.read_csv('./vehicles.csv')
+
+# Exibindo um título na página
+st.header('Dados de anúncios de vendas de carros')
+
+# Exibindo texto orientativo de funcionamento do sistema
+st.write('Selecione ao menos uma das opções abaixo para visualizar o respectivo gráfico')
 
 # Checkbox para gerar um histograma
-hist_checkbox = st.checkbox('Criar um histograma de preço geral')
+hist_checkbox = st.checkbox(
+    'Criar um histograma de preço x ano do modelo dos carros')
 
 # se o checkbox para criar histograma for clicado
 if hist_checkbox:
@@ -14,7 +22,7 @@ if hist_checkbox:
         'Criando um histograma')
 
     # Criar um histograma
-    fig = px.histogram(car_data, x='price')
+    fig = px.histogram(car_data, x='model_year', y='price', color='model')
 
     # Exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
@@ -28,7 +36,7 @@ if scatter_checkbox:
     st.write('Criando um gráfico de dispersão')
 
     # Criar um gráfico de dispersão
-    fig = px.scatter(car_data, x='model_year', y='price')
+    fig = px.scatter(car_data, x='model_year', y='price', color='model')
 
     # Exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
